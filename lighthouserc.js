d9d94a8aacb3ec8de74c0ab2fa6a12c27ebbbd14
Quinna Halim <quinnah@umich.edu>
2022-12-21 17:12:03 -0500

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,8 @@
 module.exports = {
   ci: {
     collect: {
-      staticDistDir: './',
+      url: ['http://localhost:1313/'],
+      startServerCommand: 'hugo server',
     },
     upload: {
       target: 'temporary-public-storage',
