fbc0159b34bd3bf6e4f4624254df0b42ef7966e0
Quinna Halim <quinnah@umich.edu>
2022-12-21 21:09:04 -0500

    Edit staticDistDir

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,6 @@ module.exports = {
   ci: {
     collect: {
       url: ['http://localhost:1313/'],
-      startServerCommand: 'hugo server',
       staticDistDir: './public', // hugo builds to public
     },
     upload: {
